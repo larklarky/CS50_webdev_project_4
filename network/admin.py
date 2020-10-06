f9f07d38b93fcb498768658c4b0acaecc02650eb
Olha Romanova <larklarky@gmail.com>
2020-10-06 16:59:00 +0200

    setup django admin, create models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from network.models import User, Post, Like, Following
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id', 'date_created', 'date_modified')
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user')
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('followee', 'follower')
+
+
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Following, FollowingAdmin)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    text = models.CharField(max_length = 1000, blank = False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='posts')
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
+    
+class Like(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='likes')
+
+class Following(models.Model):
+    followee = models.ForeignKey('User', on_delete=models.CASCADE, related_name='followee')
+    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name='followers')
+
+
+
+
